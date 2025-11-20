9cb6c8b1c028d456c576bd63d710db5182f768bc
Fabio <128174066+fabiosena1436@users.noreply.github.com>
2025-11-20 13:00:47 -0300

    Update catalog.js

--- catalog.js
+++ catalog.js
@@ -1,13 +1,13 @@
 const catalogTattoo = [
-    { name: 'Stitch', price: 'R$:60', src: 'assets/tatto-feita/decalque/dolirou.jpg' },
-    { name: 'Criança', price: 'R$:60', src: 'assets/tatto-feita/decalque/decalque3.jpg.png' },
-    { name: 'Lisa', price: 'R$:60', src: 'assets/tatto-feita/decalque/decalque6.jpg' },
-    { name: 'Tartaruga', price: 'R$:60', src: 'assets/tatto-feita/decalque/tartaruga.jpg' },
-    { name: 'A unidade', price: 'R$:60', src: 'assets/tatto-feita/decalque/decalque7.jpg' },
-    { name: 'Nossa senhora', price: 'R$:60', src: 'assets/tatto-feita/decalque/decalque13.jpg' },
-    { name: 'Bob esponja', price: 'R$:60', src: 'assets/tatto-feita/decalque/decalque8.jpg' },
-    { name: 'Casal', price: 'R$:60', src: 'assets/tatto-feita/decalque/decalque15.jpg' },
-    { name: 'Borboleta', price: 'R$:60', src: 'assets/tatto-feita/decalque/decalque16.jpg' },
+    { name: 'Stitch', price: 'R$:100', src: 'assets/tatto-feita/decalque/dolirou.jpg' },
+    { name: 'Criança', price: 'R$:100', src: 'assets/tatto-feita/decalque/decalque3.jpg.png' },
+    { name: 'Lisa', price: 'R$:100', src: 'assets/tatto-feita/decalque/decalque6.jpg' },
+    { name: 'Tartaruga', price: 'R$:100', src: 'assets/tatto-feita/decalque/tartaruga.jpg' },
+    { name: 'A unidade', price: 'R$:100', src: 'assets/tatto-feita/decalque/decalque7.jpg' },
+    { name: 'Nossa senhora', price: 'R$:100', src: 'assets/tatto-feita/decalque/decalque13.jpg' },
+    { name: 'Bob esponja', price: 'R$:100', src: 'assets/tatto-feita/decalque/decalque8.jpg' },
+    { name: 'Casal', price: 'R$:100', src: 'assets/tatto-feita/decalque/decalque15.jpg' },
+    { name: 'Borboleta', price: 'R$:100', src: 'assets/tatto-feita/decalque/decalque16.jpg' },
 
 ]
 
